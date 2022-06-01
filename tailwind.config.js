c032ff9c0bdcc653de2ad5c79256783d2b1f0a95
Stephen Joseph Muñez <sjtmunez@gmail.com>
2022-06-01 22:01:23 +0800

    edit src files

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        mobile: "375px",
+        tablet: "768px",
+        desktop: "1440px",
+      },
+
+      colors: {
+        yellow: "#FCB72B",
+        snow: "#F2F5F9",
+        "dark-navy": "#495567",
+        "dim-grey": "#939CAA",
+        "light-grey": "#E5ECF4",
+        "light-yellow": "#FFF4DF",
+      },
+    },
   },
   plugins: [],
 };
