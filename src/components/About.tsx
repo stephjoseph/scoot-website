7c887ea605f440a1600ff6604d6117bb218e351a
Stephen Joseph Muñez <sjtmunez@gmail.com>
2022-06-02 14:41:09 +0800

    Create About.tsx

---
+++ src/components/About.tsx
@@ -0,0 +1,13 @@
+import Header from "./about/Header";
+import Main from "./about/Main";
+
+const About = () => {
+  return (
+    <div id="about" className="flex w-full flex-col items-center">
+      <Header />
+      <Main />
+    </div>
+  );
+};
+
+export default About;
