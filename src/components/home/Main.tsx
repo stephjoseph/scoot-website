77eebdc05ae36994ec0ea5809bf05136620ef315
Stephen Joseph Muñez <sjtmunez@gmail.com>
2022-06-02 13:16:10 +0800

    Create Main.tsx

---
+++ src/components/home/Main.tsx
@@ -0,0 +1,13 @@
+import Features from "./main/Features";
+import Steps from "./main/Steps";
+
+function Main() {
+  return (
+    <main className="mb-[7.5rem] flex w-full flex-col items-center gap-[7.5rem]">
+      <Steps />
+      <Features />
+    </main>
+  );
+}
+
+export default Main;
