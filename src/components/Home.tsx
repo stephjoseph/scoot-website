91d345c5b0675d5e98f8ef06559d2a01a5620c69
Stephen Joseph Muñez <sjtmunez@gmail.com>
2022-06-02 13:16:06 +0800

    Update Home.tsx

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,9 +1,11 @@
 import Header from "./home/Header";
+import Main from "./home/Main";
 
 const Home = () => {
   return (
-    <div id="home" className="">
+    <div id="home" className="flex w-full flex-col items-center">
       <Header />
+      <Main />
     </div>
   );
 };
