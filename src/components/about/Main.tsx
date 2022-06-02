0ffd416796469a5eea6f79de858c5ad1c6265339
Stephen Joseph Muñez <sjtmunez@gmail.com>
2022-06-02 14:41:13 +0800

    Create Main.tsx

---
+++ src/components/about/Main.tsx
@@ -0,0 +1,13 @@
+import Features from "./main/Features";
+import Values from "./main/Values";
+
+const Main = () => {
+  return (
+    <main className="flex w-full flex-col items-center gap-[7.5rem]">
+      <Features />
+      <Values />
+    </main>
+  );
+};
+
+export default Main;
