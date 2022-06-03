6bf94cb1bd8a1a48ffab1ee59aaa3a25c80eda70
Stephen Joseph Muñez <sjtmunez@gmail.com>
2022-06-03 14:01:50 +0800

    Create Careers.tsx

---
+++ src/components/Careers.tsx
@@ -0,0 +1,13 @@
+import Header from "./careers/Header";
+import Main from "./careers/Main";
+
+const Careers = () => {
+  return (
+    <div id="careers" className="mb-[7.5rem] flex w-full flex-col items-center">
+      <Header />
+      <Main />
+    </div>
+  );
+};
+
+export default Careers;
