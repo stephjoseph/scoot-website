5f57bf942ca84c9a64e9a3327586319f64b8a1a1
Stephen Joseph Muñez <sjtmunez@gmail.com>
2022-06-05 21:16:24 +0800

    Create Header.tsx

---
+++ src/components/locations/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className="mb-[4.5rem] flex w-full items-center justify-center py-[3.75rem] text-center">
+      <h1 className="font-h3 leading-[2.5rem] text-white">Locations</h1>
+    </header>
+  );
+};
+
+export default Header;
