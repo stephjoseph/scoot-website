520a0b117ec177a323488e7f60be2eecf061c6cd
Stephen Joseph Muñez <sjtmunez@gmail.com>
2022-06-05 21:16:22 +0800

    Create Locations.tsx

---
+++ src/components/Locations.tsx
@@ -0,0 +1,16 @@
+import Header from "./locations/Header";
+import Main from "./locations/Main";
+
+const Locations = () => {
+  return (
+    <div
+      id="locations"
+      className="mb-[4.5rem] flex w-full flex-col items-center"
+    >
+      <Header />
+      <Main />
+    </div>
+  );
+};
+
+export default Locations;
