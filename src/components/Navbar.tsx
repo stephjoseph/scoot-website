7762465d39c301e1146349a670062c4f02a774d8
Stephen Joseph Muñez <sjtmunez@gmail.com>
2022-06-10 22:00:37 +0800

    Create Navbar.tsx

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,36 @@
+import { Link } from "react-router-dom";
+import logo from "../assets/logo.svg";
+
+const Navbar = () => {
+  return (
+    <nav className="flex items-center justify-between px-10 py-[1.375rem]">
+      <div className="flex items-center justify-between gap-[3.625rem]">
+        <Link to="/">
+          <img src={logo} alt="" />
+        </Link>
+        <ul className="flex items-center gap-8">
+          <li>
+            <Link className="font-nav" to="/about">
+              About
+            </Link>
+          </li>
+          <li>
+            <Link className="font-nav" to="/locations">
+              Location
+            </Link>
+          </li>
+          <li>
+            <Link className="font-nav" to="/careers">
+              Careers
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <Link to="" className="btn">
+        Get Scootin
+      </Link>
+    </nav>
+  );
+};
+
+export default Navbar;
