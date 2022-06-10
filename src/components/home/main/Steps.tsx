87b8ce0bffbcb82d5a08e5a9e610213f85504e36
Stephen Joseph Muñez <sjtmunez@gmail.com>
2022-06-10 22:35:35 +0800

    Update Steps.tsx

--- src/components/home/main/Steps.tsx
+++ src/components/home/main/Steps.tsx
@@ -4,13 +4,16 @@ import rideIcon from "../../../assets/icons/ride.svg";
 
 const Steps = () => {
   return (
-    <section className="flex w-[82.93%] flex-col items-center gap-12">
-      <div className="flex flex-col items-center gap-6 text-center">
-        <div className="h-[3.5rem] w-[3.5rem]">
-          <img src={locateIcon} alt="" />
+    <section className="relative z-10 flex w-[82.93%] flex-col items-center gap-12 tablet:w-[74.60%] tablet:max-w-[50rem] tablet:gap-10">
+      <div className="absolute top-[129px] left-[-208px] -z-10 hidden h-4 w-[32.063rem] rotate-90 bg-light-grey tablet:block"></div>
+      <div className="flex flex-col items-center gap-6 text-center tablet:flex-row tablet:items-start tablet:gap-20 tablet:text-left">
+        <div className="h-[3.5rem] w-[3.5rem] tablet:w-[6rem]">
+          <img className="w-full" src={locateIcon} alt="" />
         </div>
-        <div className="flex flex-col gap-6">
-          <h3 className="font-h5 text-dark-navy">Locate with app</h3>
+        <div className="flex flex-col gap-6 tablet:w-[69.45%] tablet:gap-[1.688rem]">
+          <h3 className="font-h5 tablet:font-h4 text-dark-navy">
+            Locate with app
+          </h3>
           <p className="font-body text-dim-grey">
             Use the app to find the nearest scooter to you. We are continuously
             placing scooters in the areas with most demand, so one should never
@@ -18,12 +21,14 @@ const Steps = () => {
           </p>
         </div>
       </div>
-      <div className="flex flex-col items-center gap-6 text-center">
-        <div className="h-[3.5rem] w-[3.5rem]">
-          <img src={scooterIcon} alt="" />
+      <div className="flex flex-col items-center gap-6 text-center tablet:flex-row tablet:items-start tablet:gap-20 tablet:text-left">
+        <div className="h-[3.5rem] w-[3.5rem] tablet:w-[6rem]">
+          <img className="w-full" src={scooterIcon} alt="" />
         </div>
-        <div className="flex flex-col gap-6">
-          <h3 className="font-h5 text-dark-navy">Pick your scooter</h3>
+        <div className="flex flex-col gap-6 tablet:w-[69.45%] tablet:gap-[1.688rem]">
+          <h3 className="font-h5 tablet:font-h4 text-dark-navy">
+            Pick your scooter
+          </h3>
           <p className="font-body text-dim-grey">
             We show the most important info for the scooters closest to you. So
             you know how much charge they have left and can see roughly how much
@@ -31,12 +36,14 @@ const Steps = () => {
           </p>
         </div>
       </div>
-      <div className="flex flex-col items-center gap-6 text-center">
-        <div className="h-[3.5rem] w-[3.5rem]">
-          <img src={rideIcon} alt="" />
+      <div className="flex flex-col items-center gap-6 text-center tablet:flex-row tablet:items-start tablet:gap-20 tablet:text-left">
+        <div className="h-[3.5rem] w-[3.5rem] tablet:w-[6rem]">
+          <img className="w-full" src={rideIcon} alt="" />
         </div>
-        <div className="flex flex-col gap-6">
-          <h3 className="font-h5 text-dark-navy">Enjoy the ride</h3>
+        <div className="flex flex-col gap-6 tablet:w-[69.45%] tablet:gap-[1.688rem]">
+          <h3 className="font-h5 tablet:font-h4 text-dark-navy">
+            Enjoy the ride
+          </h3>
           <p className="font-body text-dim-grey">
             Scan the QR code and the bike will unlock. Retract the cable lock,
             put on a helmet, and you’re off! Always lock bikes away from
