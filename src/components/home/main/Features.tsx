05291142bc59cd99d1de383e2ee7691ccc2d7d66
Stephen Joseph Muñez <sjtmunez@gmail.com>
2022-06-12 10:59:33 +0800

    Update Features.tsx

--- src/components/home/main/Features.tsx
+++ src/components/home/main/Features.tsx
@@ -8,22 +8,26 @@ import paymentsImg from "../../../assets/images/payments.jpg";
 // patterns
 import leftDownwardArrow from "../../../assets/patterns/left-downward-arrow.svg";
 import rightDownwardArrow from "../../../assets/patterns/right-downward-arrow.svg";
+import circle from "../../../assets/patterns/circle.svg";
 
 const Features = () => {
   return (
-    <section className="flex w-[82.93%] flex-col items-center gap-[7.5rem] text-center">
+    <section className="flex w-[82.93%] flex-col items-center gap-[7.5rem] text-center tablet:w-[74.60%]">
       <div className="flex flex-col items-center gap-14">
         <div className="relative">
           <div>
             <img className="rounded-full" src={telemetryImg} alt="" />
           </div>
-          <div className="absolute  bottom-0 h-[8.625rem] w-[45.938rem]">
+          <div className="absolute bottom-0 z-10 h-[8.625rem] w-[45.938rem] tablet:left-[-7.813rem]">
             <img className="h-full w-full" src={leftDownwardArrow} alt="" />
           </div>
+          <div className="absolute top-0 -right-[509px] hidden tablet:block">
+            <img src={circle} alt="" />
+          </div>
         </div>
-        <div className="flex flex-col items-center gap-8">
-          <div className="flex flex-col items-center gap-8">
-            <h2 className="font-['Space_Mono'] text-[2rem] leading-[2rem] tracking-[-1.43px] text-dark-navy">
+        <div className="flex flex-col items-center gap-8 tablet:gap-10">
+          <div className="flex flex-col items-center gap-8 tablet:gap-10">
+            <h2 className="tablet:font-h2 font-['Space_Mono'] text-[2rem] leading-[2rem] tracking-[-1.43px] text-dark-navy tablet:max-w-[28.563rem]">
               Easy to use riding telemetry
             </h2>
             <p className="font-body text-dim-grey">
@@ -44,13 +48,16 @@ const Features = () => {
           <div>
             <img className="rounded-full" src={nearYouImg} alt="" />
           </div>
-          <div className="absolute  top-0 -left-[31.563rem] ">
+          <div className="absolute  top-0 -left-[31.563rem] tablet:-left-[550px]">
             <img className="h-full w-full" src={rightDownwardArrow} alt="" />
           </div>
+          <div className="absolute top-0 -left-[509px] hidden tablet:block">
+            <img src={circle} alt="" />
+          </div>
         </div>
-        <div className="flex flex-col items-center gap-8">
-          <div className="flex flex-col items-center gap-8">
-            <h2 className="font-['Space_Mono'] text-[2rem] leading-[2rem] tracking-[-1.43px] text-dark-navy">
+        <div className="flex flex-col items-center gap-8 tablet:gap-10">
+          <div className="flex flex-col items-center gap-8 tablet:gap-10">
+            <h2 className="tablet:font-h2 font-['Space_Mono'] text-[2rem] leading-[2rem] tracking-[-1.43px] text-dark-navy tablet:max-w-[28.563rem]">
               Coming to a city near you
             </h2>
             <p className="font-body text-dim-grey">
@@ -71,13 +78,16 @@ const Features = () => {
           <div>
             <img className="rounded-full" src={paymentsImg} alt="" />
           </div>
-          <div className="absolute top-0 left-[8.125rem] h-[8.625rem] w-[45.938rem]">
+          <div className="absolute top-0 left-[8.125rem] z-10 h-[8.625rem] w-[45.938rem] tablet:left-[248px] tablet:top-[85px]">
             <img className="h-full w-full" src={leftDownwardArrow} alt="" />
           </div>
+          <div className="absolute top-0 -right-[509px] hidden tablet:block">
+            <img src={circle} alt="" />
+          </div>
         </div>
-        <div className="flex flex-col items-center gap-8">
-          <div className="flex flex-col items-center gap-8">
-            <h2 className="font-['Space_Mono'] text-[2rem] leading-[2rem] tracking-[-1.43px] text-dark-navy">
+        <div className="flex flex-col items-center gap-8 tablet:gap-10">
+          <div className="flex flex-col items-center gap-8 tablet:gap-10">
+            <h2 className="tablet:font-h2 font-['Space_Mono'] text-[2rem] leading-[2rem] tracking-[-1.43px] text-dark-navy tablet:max-w-[28.563rem]">
               Zero hassle payments
             </h2>
             <p className="font-body text-dim-grey">
