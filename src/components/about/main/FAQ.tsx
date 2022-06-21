6550bcd18747c0f4e36c0626095350f7a4005d49
stephjoseph <sjtmunez@gmail.com>
2022-06-21 20:42:32 +0800

    Update FAQ.tsx

--- src/components/about/main/FAQ.tsx
+++ src/components/about/main/FAQ.tsx
@@ -1,7 +1,7 @@
-import { useState } from "react";
+import { useState } from 'react';
 
-import data from "./data";
-import chevronIcon from "../../../assets/icons/chevron.svg";
+import data from './data';
+import chevronIcon from '../../../assets/icons/chevron.svg';
 
 const FAQ = () => {
   const [selected, setSelected] = useState<number | null>(0);
@@ -19,72 +19,72 @@ const FAQ = () => {
     console.log(data.faq1.map((item) => item.question));
   };
   return (
-    <section className="flex w-[82.93%] flex-col items-center gap-12">
+    <section className='flex w-[82.93%] flex-col items-center gap-12 tablet:w-[89.71%] tablet:gap-16'>
       <h2
         onClick={logging}
-        className="font-h2 text-[2rem] leading-[2rem] tracking-[-1.43px] text-dark-navy"
+        className='font-h2 tablet:font-h2 text-[2rem] leading-[2rem] tracking-[-1.43px] text-dark-navy'
       >
         FAQs
       </h2>
-      <div className="flex w-full flex-col items-center gap-12">
-        <div className="flex flex-col items-center gap-8">
-          <h3 className="font-h3 text-2xl leading-[1.75rem] tracking-[-1.07px] text-dark-navy">
+      <div className='flex w-full flex-col items-center gap-12'>
+        <div className='flex flex-col items-center gap-8'>
+          <h3 className='font-h3 tablet:font-h3 text-2xl leading-[1.75rem] tracking-[-1.07px] text-dark-navy'>
             How it works
           </h3>
-          <div className="flex flex-col items-center gap-4">
+          <div className='flex flex-col items-center gap-4 tablet:gap-6'>
             {data.faq1.map((item, i) => (
               <div
-                className="flex flex-col bg-snow p-8"
+                className='flex flex-col bg-snow p-8'
                 key={item.id.toString()}
               >
                 <div
-                  className="flex items-center justify-between"
+                  className='flex items-center justify-between'
                   onClick={() => toggle(i)}
                 >
-                  <h4 className="font-h4 max-w-[13.688rem] cursor-pointer  text-lg leading-6 tracking-[-0.8px] text-dark-navy">
+                  <h4 className='font-h4 tablet:font-h4 max-w-[13.688rem] cursor-pointer text-lg leading-6 tracking-[-0.8px] text-dark-navy tablet:max-w-full'>
                     {item.question}
                   </h4>
                   <div
                     className={`${
-                      selected === i ? "rotate-180" : ""
+                      selected === i ? 'rotate-180' : ''
                     } cursor-pointer transition-[transform] duration-300 ease-in`}
                   >
-                    <img src={chevronIcon} alt="" />
+                    <img src={chevronIcon} alt='' />
                   </div>
                 </div>
-                <p className={`${selected === i ? "answer show" : "answer"}`}>
+                <p className={`${selected === i ? 'answer show' : 'answer'}`}>
                   {item.answer}
                 </p>
               </div>
             ))}
           </div>
         </div>
-        <div className="flex flex-col items-center gap-8">
-          <h3 className="font-h3 text-2xl leading-[1.75rem] tracking-[-1.07px] text-dark-navy">
+        <div className='flex flex-col items-center gap-8'>
+          <h3 className='font-h3 tablet:font-h3 text-2xl leading-[1.75rem] tracking-[-1.07px] text-dark-navy'>
             Safe driving
           </h3>
-          <div className="flex flex-col items-center gap-4">
+          <div className='flex flex-col items-center gap-4'>
             {data.faq2.map((item, i) => (
               <div
-                className="flex flex-col bg-snow p-8"
+                className='flex flex-col bg-snow p-8'
                 key={item.id.toString()}
               >
                 <div
-                  className="flex items-center justify-between"
+                  className='flex items-center justify-between'
                   onClick={() => toggle2(i)}
                 >
-                  <h4 className="font-h4 max-w-[13.688rem] cursor-pointer text-lg leading-6 tracking-[-0.8px] text-dark-navy">
+                  <h4 className='font-h4 tablet:font-h4 max-w-[13.688rem] cursor-pointer text-lg leading-6 tracking-[-0.8px] text-dark-navy tablet:max-w-full'>
                     {item.question}
                   </h4>
                   <div
                     className={`${
-                      selected2 === i ? "rotate-180" : ""
+                      selected2 === i ? 'rotate-180' : ''
                     } cursor-pointer transition-[transform] duration-300 ease-in`}
                   >
-                    <img src={chevronIcon} alt="" />
+                    <img src={chevronIcon} alt='' />
                   </div>
                 </div>
-                <p className={`${selected2 === i ? "answer show" : "answer"}`}>
+                <p className={`${selected2 === i ? 'answer show' : 'answer'}`}>
                   {item.answer}
                 </p>
               </div>
