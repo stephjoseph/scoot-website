15dfa6d31d01c3c840adc84c62d376d83c242e87
stephjoseph <sjtmunez@gmail.com>
2022-06-21 20:01:36 +0800

    Revert "Update Header.tsx"
    This reverts commit 9070122a980c3d317ff2d87902f236670ca7e026.

--- src/components/about/Header.tsx
+++ src/components/about/Header.tsx
@@ -1,14 +1,7 @@
-import whiteCircles from '../../assets/patterns/white-circles.svg';
-
 const Header = () => {
   return (
-    <header className='mb-[4.5rem] flex w-full items-center justify-center py-[3.75rem] text-center tablet:relative tablet:mb-36 tablet:justify-start tablet:py-[4.5rem] tablet:pl-24 tablet:text-left'>
-      <h1 className='font-h3 tablet:font-h1 leading-[2.5rem] text-white'>
-        About
-      </h1>
-      <div className='absolute tablet:-right-[1.938rem]'>
-        <img src={whiteCircles} alt='' />
-      </div>
+    <header className="mb-[4.5rem] flex w-full items-center justify-center py-[3.75rem] text-center">
+      <h1 className="font-h3 leading-[2.5rem] text-white">About</h1>
     </header>
   );
 };
