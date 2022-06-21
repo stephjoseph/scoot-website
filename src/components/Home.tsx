7cbb409eed5aae36f70016fcb7bf18d671848331
stephjoseph <sjtmunez@gmail.com>
2022-06-21 19:48:24 +0800

    Update Home.tsx

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,9 +1,9 @@
-import Header from "./home/Header";
-import Main from "./home/Main";
+import Header from './home/Header';
+import Main from './home/Main';
 
 const Home = () => {
   return (
-    <div id="home" className="flex w-full flex-col items-center">
+    <div id='home' className='flex w-full flex-col items-center'>
       <Header />
       <Main />
     </div>
