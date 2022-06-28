4403b7b1cf9ee158d7647dee4c586da2af5d308a
stephjoseph <sjtmunez@gmail.com>
2022-06-28 20:27:49 +0800

    Update Header.tsx

--- src/components/careers/Header.tsx
+++ src/components/careers/Header.tsx
@@ -1,7 +1,14 @@
+import whiteCircles from '../../assets/patterns/white-circles.svg';
+
 const Header = () => {
   return (
-    <header className="mb-[4.5rem] flex w-full items-center justify-center py-[3.75rem] text-center">
-      <h1 className="font-h3 leading-[2.5rem] text-white">Careers</h1>
+    <header className='mb-[4.5rem] flex w-full items-center justify-center py-[3.75rem] text-center tablet:relative tablet:mb-36 tablet:justify-start tablet:py-[4.5rem] tablet:pl-24'>
+      <h1 className='font-h3 tablet:font-h1 leading-[2.5rem] text-white'>
+        Careers
+      </h1>
+      <div className='hidden tablet:absolute tablet:-right-[1.938rem] tablet:block'>
+        <img src={whiteCircles} alt='' />
+      </div>
     </header>
   );
 };
