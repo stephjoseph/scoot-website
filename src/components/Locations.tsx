acf77033fae4048a4d494eb4a1edc73fa1c0d9a4
stephjoseph <sjtmunez@gmail.com>
2022-06-28 20:40:46 +0800

    Update Locations.tsx

--- src/components/Locations.tsx
+++ src/components/Locations.tsx
@@ -1,11 +1,11 @@
-import Header from "./locations/Header";
-import Main from "./locations/Main";
+import Header from './locations/Header';
+import Main from './locations/Main';
 
 const Locations = () => {
   return (
     <div
-      id="locations"
-      className="mb-[4.5rem] flex w-full flex-col items-center"
+      id='locations'
+      className='mb-[4.5rem] flex w-full flex-col items-center tablet:mb-[7.5rem]'
     >
       <Header />
       <Main />
