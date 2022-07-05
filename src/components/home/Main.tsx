5e06dee855575fcb8c04081aadba4ca4d17e880a
stephjoseph <sjtmunez@gmail.com>
2022-07-05 21:12:39 +0800

    Update Main.tsx

--- src/components/home/Main.tsx
+++ src/components/home/Main.tsx
@@ -1,9 +1,9 @@
-import Features from "./main/Features";
-import Steps from "./main/Steps";
+import Features from './main/Features';
+import Steps from './main/Steps';
 
 function Main() {
   return (
-    <main className="mb-[7.5rem] flex w-full flex-col items-center gap-[7.5rem] tablet:gap-[9.063rem]">
+    <main className='mb-[7.5rem] flex w-full flex-col items-center gap-[7.5rem] tablet:gap-[9.063rem] desktop:gap-[12.5rem]'>
       <Steps />
       <Features />
     </main>
