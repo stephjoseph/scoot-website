fe4040e0e8ea4365f0e8fa83d28fe9339de42d4a
stephjoseph <sjtmunez@gmail.com>
2022-07-13 20:14:16 +0800

    Update CTA.tsx

--- src/components/CTA.tsx
+++ src/components/CTA.tsx
@@ -8,7 +8,7 @@ import googlePlayIcon from '../assets/icons/google-play.svg';
 const CTA = () => {
   return (
     <section
-      className='flex w-full flex-col items-center gap-10 bg-dark-navy py-[5.5rem] text-center tablet:py-[3.875rem] desktop:flex-row desktop:justify-between desktop:py-[6.375rem] desktop:px-[10.313rem]'
+      className='flex w-full flex-col items-center gap-10 bg-dark-navy py-[5.5rem] text-center tablet:py-[3.875rem] desktop:flex-row desktop:justify-between desktop:py-[6.375rem] desktop:px-[10.313rem] desktop:text-left'
       id='CTA'
     >
       <h2 className="tablet:font-h2 w-[82.93%] font-['Space_Mono'] text-[2rem] leading-[2rem] tracking-[-1.43px] text-white tablet:max-w-[28.563rem]">
