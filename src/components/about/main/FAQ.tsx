aab277adefe5a9b63b655d23fe2a55bc66c2bcb0
stephjoseph <sjtmunez@gmail.com>
2022-07-13 20:14:10 +0800

    Update FAQ.tsx

--- src/components/about/main/FAQ.tsx
+++ src/components/about/main/FAQ.tsx
@@ -19,19 +19,19 @@ const FAQ = () => {
     console.log(data.faq1.map((item) => item.question));
   };
   return (
-    <section className='flex w-[82.93%] flex-col items-center gap-12 tablet:w-[89.71%] tablet:gap-16'>
+    <section className='flex w-[82.93%] flex-col items-center gap-12 tablet:w-[89.71%] tablet:gap-16 desktop:w-[77.08%] desktop:gap-16'>
       <h2
         onClick={logging}
         className='font-h2 tablet:font-h2 text-[2rem] leading-[2rem] tracking-[-1.43px] text-dark-navy'
       >
         FAQs
       </h2>
-      <div className='flex w-full flex-col items-center gap-12'>
-        <div className='flex flex-col items-center gap-8'>
-          <h3 className='font-h3 tablet:font-h3 text-2xl leading-[1.75rem] tracking-[-1.07px] text-dark-navy'>
+      <div className='flex w-full flex-col items-center gap-12 desktop:gap-16'>
+        <div className='flex flex-col items-center gap-8 desktop:flex-row desktop:items-start desktop:gap-[1.875rem]'>
+          <h3 className='font-h3 tablet:font-h3 text-2xl leading-[1.75rem] tracking-[-1.07px] text-dark-navy desktop:w-[31.53%]'>
             How it works
           </h3>
-          <div className='flex flex-col items-center gap-4 tablet:gap-6'>
+          <div className='flex flex-col items-center gap-4 tablet:gap-6 desktop:w-[65.77%] desktop:gap-4'>
             {data.faq1.map((item, i) => (
               <div
                 className='flex w-full flex-col bg-snow p-8'
@@ -59,11 +59,11 @@ const FAQ = () => {
             ))}
           </div>
         </div>
-        <div className='flex flex-col items-center gap-8'>
-          <h3 className='font-h3 tablet:font-h3 text-2xl leading-[1.75rem] tracking-[-1.07px] text-dark-navy'>
+        <div className='flex flex-col items-center gap-8 desktop:flex-row desktop:items-start desktop:gap-[1.875rem]'>
+          <h3 className='font-h3 tablet:font-h3 text-2xl leading-[1.75rem] tracking-[-1.07px] text-dark-navy desktop:w-[31.53%]'>
             Safe driving
           </h3>
-          <div className='flex flex-col items-center gap-4'>
+          <div className='flex flex-col items-center gap-4 desktop:w-[65.77%] desktop:gap-4'>
             {data.faq2.map((item, i) => (
               <div
                 className='flex flex-col bg-snow p-8'
