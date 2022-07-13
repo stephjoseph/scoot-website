f2d68a0ee512480f07757d2b54307cbbd5718d52
stephjoseph <sjtmunez@gmail.com>
2022-07-13 20:50:04 +0800

    Update Careers.tsx

--- src/components/Careers.tsx
+++ src/components/Careers.tsx
@@ -1,9 +1,12 @@
-import Header from "./careers/Header";
-import Main from "./careers/Main";
+import Header from './careers/Header';
+import Main from './careers/Main';
 
 const Careers = () => {
   return (
-    <div id="careers" className="mb-[7.5rem] flex w-full flex-col items-center">
+    <div
+      id='careers'
+      className='mb-[7.5rem] flex w-full flex-col items-center desktop:mb-[10rem]'
+    >
       <Header />
       <Main />
     </div>
