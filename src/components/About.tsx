5a29a18261d410bf5c6d5e30fc177e4c609b74ea
stephjoseph <sjtmunez@gmail.com>
2022-07-13 20:14:08 +0800

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,9 +1,12 @@
-import Header from "./about/Header";
-import Main from "./about/Main";
+import Header from './about/Header';
+import Main from './about/Main';
 
 const About = () => {
   return (
-    <div id="about" className="mb-[7.5rem] flex w-full flex-col items-center">
+    <div
+      id='about'
+      className='mb-[7.5rem] flex w-full flex-col items-center desktop:mb-[10rem]'
+    >
       <Header />
       <Main />
     </div>
